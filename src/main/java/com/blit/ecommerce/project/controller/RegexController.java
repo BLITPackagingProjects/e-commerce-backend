f2ea13a59dd0029ee62760e47bd6c0e2a3cf7a23
qwqw1111 <haycockyoung@gmail.com>
2023-11-17 13:12:49 -0500

    added crossOrigin annotation

--- src/main/java/com/blit/ecommerce/project/controller/RegexController.java
+++ src/main/java/com/blit/ecommerce/project/controller/RegexController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,6 +27,7 @@ public class RegexController {
 	ProductService pServ;
 	
 	@GetMapping("/regex")
+	@CrossOrigin
 	public ResponseEntity<List<Product>> regexProducts(@RequestHeader String regex){
 		System.out.println(regex);
 		return new ResponseEntity<>(pServ.regexProducts(regex),HttpStatus.OK);
