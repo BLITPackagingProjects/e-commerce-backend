5a492b15fc27d71549de671a69419fa23f1e8fe7
qwqw1111 <haycockyoung@gmail.com>
2023-11-30 22:20:25 -0500

    some order functionality (like find active order) didn't make it to the final version, so rather than risk a merge, I'm just copying and pasting the relevant lines

--- src/main/java/com/blit/ecommerce/project/repository/OrderRepository.java
+++ src/main/java/com/blit/ecommerce/project/repository/OrderRepository.java
@@ -16,4 +16,7 @@ public interface OrderRepository extends CrudRepository<OrderDetail, Long> {
 
 	@Query(nativeQuery = true, value = "select order_detail.* from order_detail left join order_products on order_detail.order_id=order_products.order_id where product_id = ?1")
 	List<OrderDetail> findOrdersByProductId(Long product_id);
+	
+	@Query(nativeQuery = true, value = "select order_detail.* from order_detail WHERE user_id= ?1 AND canceled=0 order by date DESC LIMIT 1")
+	OrderDetail findActiveOrderByUserId(Long userId);
 }
