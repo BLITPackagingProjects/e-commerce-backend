5325b6cb5eab7338d72a25f464c8762eedf71a3b
qwqw1111 <haycockyoung@gmail.com>
2023-11-30 22:23:31 -0500

    Order Controller

--- src/main/java/com/blit/ecommerce/project/controller/OrderController.java
+++ src/main/java/com/blit/ecommerce/project/controller/OrderController.java
@@ -40,6 +40,26 @@ public class OrderController {
         orderService.createOrder(userId, orderDetail);
         return new ResponseEntity<>("Order is created successfully", HttpStatus.CREATED);
     }
+    
+    @PostMapping("/checkout/{userId}")
+    public ResponseEntity<String> createOrder(
+            @PathVariable long userId){
+        orderService.checkout(userId);
+        return new ResponseEntity<>("Checkout successful", HttpStatus.CREATED);
+    }
+    
+    @GetMapping("/active/{userId}")
+    public ResponseEntity<OrderDetail> findActiveOrder(
+            @PathVariable long userId){
+        
+        return new ResponseEntity<>(orderService.findActiveOrderByUserId(userId), HttpStatus.OK);
+    }
+    
+    @DeleteMapping("/cancel/{orderId}")
+    public ResponseEntity<String> cancelOrder(@PathVariable("orderId") long orderId){
+    	orderService.cancelOrder(orderId);
+    	return new ResponseEntity<>("Order cancelled!",HttpStatus.ACCEPTED);
+    }
 
     @PostMapping("/{orderId}/{productId}")
     public ResponseEntity<String> getOrder(
